ce38f09eaae653943916572778860e8eaa048bb9
Sam D <samidiaz3@gmail.com>
2021-02-28 20:01:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+
+attr_accessor
+attr_reader
+
+
+  def initialize (school)
+    @school = school
+    @roster = {}
+  end
+
+def roster
+  @roster = {}
+end
+
+  def school
+    @school
+  #@roster
+  end
+
+
+end
