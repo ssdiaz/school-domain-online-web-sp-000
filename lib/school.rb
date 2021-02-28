8a54c675905731f56dd71d4c6c8466780642b492
Sam D <samidiaz3@gmail.com>
2021-02-28 20:38:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,24 +7,19 @@ attr_reader :school, :roster
 
   def initialize (school)
     @school = school
-    @roster = {} #key (grade), value(array of names)
+    @roster = {} #key (grade), value ([array] of names)
   end
 
-  # def school
-  #   @school
-  # end
-  #
-  # def roster
-  #   @roster = {}
-  # end
-
   def add_student (name, grade)
-  #first, create the key set to an array, then << in name
-    #roster[grade] = []
-  #but what if array already has data? Don't want to erase data.
-    # use ||= to day use roster[grade] or make new empty array if does not yet exist
+    #first, create the key set to an array, then << in name
+      #roster[grade] = []
+    #but what if array already has data? Don't want to erase data.
+      # use ||= to day use roster[grade] or make new empty array if does not yet exist
     roster[grade] ||= []
     roster[grade] << name
+  end
+
+  def grade
   end
 
 
